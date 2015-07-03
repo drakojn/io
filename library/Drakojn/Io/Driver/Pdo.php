ecc64f0eb85056a47a56fa03e02efb975a1272da
Anderson 'duodraco' Casimiro <duodraco@gmail.com>
2015-07-03 15:12:39 -0300

    0.2.1 - fixes for update and select - breaks when looking for remote keys instead local ones

--- library/Drakojn/Io/Driver/Pdo.php
+++ library/Drakojn/Io/Driver/Pdo.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Drakojn\Io\Driver;
 
 use Drakojn\Io\Mapper;
@@ -10,9 +9,15 @@ use Drakojn\Io\Driver\SQL\Select;
 use Drakojn\Io\Driver\SQL\Update;
 use Drakojn\Io\Driver\SQL\Delete;
 
+/**
+ * Class Pdo
+ *
+ * @package Drakojn\Io\Driver
+ */
 class Pdo implements DriverInterface
 {
     /**
+     * PDO
      *
      * @var PHPDataObject
      */
@@ -20,44 +25,61 @@ class Pdo implements DriverInterface
 
     /**
      * Constructor.
-     * 
-     * @param PHPDataObject $pdo
+     *
+     * @param PHPDataObject $pdo a Pdo instance
      */
     public function __construct(PHPDataObject $pdo)
     {
         $this->pdo = $pdo;
     }
 
+    /**
+     * Searches for object matching $query
+     *
+     * @param Mapper $mapper the Mapper
+     * @param array  $query  the Query
+     *
+     * @return array
+     */
     public function find(Mapper $mapper, array $query = [])
     {
         $iterator = new Select($this->pdo, $mapper, $query);
         return $iterator->getStatement()->fetchAll();
     }
 
+    /**
+     * Persists $object
+     *
+     * @param Mapper $mapper Mapper
+     * @param object $object Object to be saved
+     *
+     * @return Insert|Update
+     */
     public function save(Mapper $mapper, $object)
     {
         $identifier = $mapper->getMap()->getIdentifier();
         $data       = $mapper->getMap()->getData($object);
-        
         if ($data[$identifier]) {
-            return $this->update($mapper, $object, $data);
+            return $this->update($mapper, $data);
         }
-        
         return $this->insert($mapper, $object, $data);
     }
 
     protected function insert(Mapper $mapper, $object, array $data)
     {
-        return new Insert($this->pdo, $mapper, $object, $data);
+        $object = new Insert($this->pdo, $mapper, $object, $data);
+        return $object();
     }
 
     protected function update(Mapper $mapper, array $data)
     {
-        return new Update($this->pdo, $mapper, $data, $condition);
+        $object = new Update($this->pdo, $mapper, $data);
+        return $object();
     }
-    
+
     public function delete(Mapper $mapper, $object)
     {
-        return new Delete($this->pdo, $mapper, $object);
+        $object = new Delete($this->pdo, $mapper, $object);
+        return $object();
     }
 }
